40af2b8df80342fe4a788d75c3bfce90cdfdb63c
Amy Fogelman <abfnj1@gmail.com>
2019-05-08 18:35:33 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def #introduction_with_language(amy, ruby)
+def introduction_with_language (Amy, Ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
